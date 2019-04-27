5d14081ae880dbcf0201b055fd404746050b4292
augustin <cheron.augustin@gmail.com>
2019-04-27 21:12:53 +0200

    split f32 and f64 module, running openlibm tests and bench

---
+++ libm/src/lib_f64.rs
@@ -0,0 +1,357 @@
+use super::signgam;
+use core::{f64, i32};
+use libc::{c_double, c_int, c_long, c_longlong};
+use libm_internals;
+
+#[no_mangle]
+pub extern "C" fn acos(arg: c_double) -> c_double {
+    libm_internals::acos(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn acosh(arg: c_double) -> c_double {
+    libm_internals::acosh(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn asin(arg: c_double) -> c_double {
+    libm_internals::asin(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn asinh(arg: c_double) -> c_double {
+    libm_internals::asinh(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn atan(arg: c_double) -> c_double {
+    libm_internals::atan(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn atan2(y: c_double, x: c_double) -> c_double {
+    libm_internals::atan2(y, x)
+}
+
+#[no_mangle]
+pub extern "C" fn atanh(arg: c_double) -> c_double {
+    libm_internals::atanh(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn cbrt(arg: c_double) -> c_double {
+    libm_internals::cbrt(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn ceil(arg: c_double) -> c_double {
+    libm_internals::ceil(arg)
+}
+
+
+#[no_mangle]
+pub extern "C" fn cos(arg: c_double) -> c_double {
+    libm_internals::cos(arg)
+}
+
+
+#[no_mangle]
+pub extern "C" fn cosh(arg: c_double) -> c_double {
+    libm_internals::cosh(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn exp(arg: c_double) -> c_double {
+    libm_internals::exp(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn exp2(arg: c_double) -> c_double {
+    libm_internals::exp2(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn expm1(arg: c_double) -> c_double {
+    libm_internals::expm1(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn erf(arg: c_double) -> c_double {
+    libm_internals::erf(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn erfc(arg: c_double) -> c_double {
+    libm_internals::erf(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn fabs(arg: c_double) -> c_double {
+    libm_internals::fabs(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn fdim(x: c_double, y: c_double) -> c_double {
+    libm_internals::fdim(x, y)
+}
+
+
+#[no_mangle]
+pub extern "C" fn floor(arg: c_double) -> c_double {
+    libm_internals::floor(arg)
+}
+
+
+#[no_mangle]
+pub extern "C" fn fma(x: c_double, y: c_double, z: c_double) -> c_double {
+    libm_internals::fma(x, y, z)
+}
+
+#[no_mangle]
+pub extern "C" fn fmod(numer: c_double, denom: c_double) -> c_double {
+    libm_internals::fmod(numer, denom)
+}
+
+#[no_mangle]
+pub extern "C" fn fmax(x: c_double, y: c_double) -> c_double {
+    (x as f64).max(y)
+}
+
+#[no_mangle]
+pub extern "C" fn fmin(x: c_double, y: c_double) -> c_double {
+    (x as f64).min(y)
+}
+
+#[no_mangle]
+pub extern "C" fn hypot(x: c_double, y: c_double) -> c_double {
+    libm_internals::hypot(x, y)
+}
+
+#[no_mangle]
+pub extern "C" fn log(arg: c_double) -> c_double {
+    libm_internals::log(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn log10(arg: c_double) -> c_double {
+    libm_internals::log10(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn log1p(arg: c_double) -> c_double {
+    libm_internals::log1p(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn log2(arg: c_double) -> c_double {
+    libm_internals::log2(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn pow(base: c_double, exponent: c_double) -> c_double {
+    libm_internals::pow(base, exponent)
+}
+
+#[no_mangle]
+pub extern "C" fn round(arg: c_double) -> c_double {
+    libm_internals::round(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn scalbn(x: c_double, n: c_int) -> c_double {
+    libm_internals::scalbn(x, n)
+}
+
+#[no_mangle]
+pub extern "C" fn sin(arg: c_double) -> c_double {
+    libm_internals::sin(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn sinh(arg: c_double) -> c_double {
+    libm_internals::sinh(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn sqrt(arg: c_double) -> c_double {
+    libm_internals::sqrt(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn tan(arg: c_double) -> c_double {
+    libm_internals::tan(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn tanh(arg: c_double) -> c_double {
+    libm_internals::tanh(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn trunc(arg: c_double) -> c_double {
+    libm_internals::trunc(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn lgamma(arg: c_double) -> c_double {
+    let (res, err) = libm_internals::lgamma_r(arg);
+    unsafe { signgam = err };
+    res
+}
+#[no_mangle]
+pub extern "C" fn tgamma(arg: c_double) -> c_double {
+    libm_internals::tgamma(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn ilogb(arg: c_double) -> c_int {
+    libm_internals::ilogb(arg) as c_int
+}
+
+#[no_mangle]
+pub extern "C" fn llround(arg: c_double) -> c_longlong {
+    libm_internals::round(arg) as c_longlong
+}
+
+#[no_mangle]
+pub extern "C" fn lround(arg: c_double) -> c_long {
+    libm_internals::round(arg) as c_long
+}
+
+#[no_mangle]
+pub extern "C" fn llrint(arg: c_double) -> c_longlong {
+    libm_internals::rint(arg) as c_longlong
+}
+
+#[no_mangle]
+pub extern "C" fn lrint(arg: c_double) -> c_long {
+    libm_internals::rint(arg) as c_long
+}
+
+#[no_mangle]
+pub extern "C" fn rint(arg: c_double) -> c_double {
+    libm_internals::rint(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn nearbyint(arg: c_double) -> c_double {
+    // FIXME: should not rise error
+    libm_internals::rint(arg)
+}
+
+#[no_mangle]
+pub extern "C" fn sincos(x: c_double, sin: *mut c_double, cos: *mut c_double) {
+    let (rsin, rcos) = libm_internals::sincos(x);
+    unsafe {
+        *sin = rsin;
+        *cos = rcos;
+    }
+}
+// fixme should probably be in internals
+#[no_mangle]
+pub extern "C" fn scalbln(x: c_double, n: c_long) -> c_double {
+    if n > i32::max_value() as i64 {
+        libm_internals::scalbn(x, i32::max_value())
+    } else if n < i32::min_value() as i64 {
+        libm_internals::scalbn(x, i32::min_value())
+    } else {
+        libm_internals::scalbn(x, n as i32)
+    }
+}
+
+// fixme should probably be in internals
+#[no_mangle]
+pub extern "C" fn logb(x: c_double) -> c_double {
+    if (x as f64).is_infinite() {
+        return x * x;
+    }
+    if x == 0.0 {
+        return -1.0 / (x * x);
+    }
+    return libm_internals::ilogb(x) as f64;
+}
+
+#[no_mangle]
+pub extern "C" fn j0(x: c_double) -> c_double {
+    return libm_internals::j0(x);
+}
+
+#[no_mangle]
+pub extern "C" fn y0(x: c_double) -> c_double {
+    return libm_internals::y0(x);
+}
+
+#[no_mangle]
+pub extern "C" fn j1(x: c_double) -> c_double {
+    return libm_internals::j1(x);
+}
+
+#[no_mangle]
+pub extern "C" fn y1(x: c_double) -> c_double {
+    return libm_internals::y1(x);
+}
+
+#[no_mangle]
+pub extern "C" fn jn(n: c_int, x: c_double) -> c_double {
+    return libm_internals::jn(n as isize, x);
+}
+
+#[no_mangle]
+pub extern "C" fn yn(n: c_int, x: c_double) -> c_double {
+    return libm_internals::yn(n as isize, x);
+}
+
+#[no_mangle]
+pub extern "C" fn nextafter(x: c_double, y: c_double) -> c_double {
+    return libm_internals::nextafter(x, y);
+}
+
+#[no_mangle]
+pub extern "C" fn remquo(numer: c_double, denom: c_double, quot: *mut c_int) -> c_double {
+    let (res, q) = libm_internals::remquo(numer, denom);
+    unsafe {
+        *quot = q as i32;
+    }
+    res
+}
+
+#[no_mangle]
+pub extern "C" fn remainder(numer: c_double, denom: c_double) -> c_double {
+    libm_internals::remquo(numer, denom).0
+}
+
+// newlib test cfg ?
+#[no_mangle]
+pub extern "C" fn __isfinite(x: c_double) -> c_int {
+    if (x as f64).is_finite() {
+        1
+    } else {
+        0
+    }
+}
+#[no_mangle]
+pub extern "C" fn __isnormal(x: c_double) -> c_int {
+    if (x as f64).is_normal() {
+        1
+    } else {
+        0
+    }
+}
+#[no_mangle]
+pub extern "C" fn __fpclassifyd(x: c_double) -> c_int {
+    const FP_NORMAL: i32 = 0x4;
+    const FP_INFINITE: i32 = 0x1;
+    const FP_ZERO: i32 = 0x10;
+    const FP_SUBNORMAL: i32 = 0x8;
+    const FP_NAN: i32 = 0x2;
+
+    let u = (x as f64).to_bits();
+    let e = u >> 52 & 0x7ff;
+    if e == 0 {
+        return if u << 1 != 0 { FP_SUBNORMAL } else { FP_ZERO };
+    }
+    if e == 0x7ff {
+        return if u << 12 != 0 { FP_NAN } else { FP_INFINITE };
+    }
+    return FP_NORMAL;
+}
--- libm/tests/test.sh
+++ libm/tests/test.sh
@@ -2,9 +2,21 @@
 
 #echo [+] Run musl test suite
 #cd tests/musl-test/libc-test/
-#    make --silent > /dev/null 2> /dev/null;
+#    make --silent;
 #    cat math/REPORT
 #cd -
 
 echo [+] Run openlibm test suite
-cd tests/openlibm-test/openlibm/test && make
+cd tests/openlibm-test/openlibm/test
+    make
+    make bench
+    chmod +x test-float test-double bench-openlibm bench-syslibm
+    ./test-double
+    ./test-float
+    echo ===BENCH relibm ===
+    ./bench-openlibm
+    echo ===BENCH syslibm ===
+    ./bench-syslibm
+cd -
+
+
