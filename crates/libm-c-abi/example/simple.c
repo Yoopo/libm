b1da814f6fb3cdaec7f221211c6ce1098a6c8328
augustin <cheron.augustin@gmail.com>
2020-01-27 20:06:03 +0100

    add some comment in simple.c

--- crates/libm-c-abi/example/simple.c
+++ crates/libm-c-abi/example/simple.c
@@ -1,3 +1,8 @@
+/* 
+* Simple test program.
+* You can use it to debug test case outside the test suite.
+* This is usefull to debug in parallel the C and rust version, to float debug precision issue.
+*/
 #include <math.h>
 #include <stdio.h>
 
